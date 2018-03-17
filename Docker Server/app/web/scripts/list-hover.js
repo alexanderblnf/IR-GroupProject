38845f5822ee2f675c2e51f6095f892c65abe1e2
alexbln <alexandru@g2g-online.nl>
2018-03-17 18:52:45 +0100

    Added tooltip

--- Docker Server/app/web/scripts/list-hover.js
+++ Docker Server/app/web/scripts/list-hover.js
@@ -1,3 +1,13 @@
+function createTooltip(text, description) {
+	var a = document.createElement('a');
+	a.href = '#';
+	a.setAttribute('data-toggle', 'tooltip');
+	a.title = description;
+	a.innerHTML = text;
+
+	return a;
+}
+
 $(document).on('click', '#search-button', function () {
 	var query = $('#query-input').val();
 	var response = $.ajax({
@@ -6,7 +16,6 @@ $(document).on('click', '#search-button', function () {
 	});
 
 	response.done(function (res) {
-		console.log(res);
 		var result = res;
 		if (result.code !== 200) {
 			console.log(result.response);
@@ -29,16 +38,19 @@ $(document).on('click', '#search-button', function () {
 			// a.className = 'a-title';
 
 			// Used for summary
-			var divInner = document.createElement('div');
+			// var divInner = document.createElement('div');
 			var span = document.createElement('span');
-			span.innerHTML = "DESCRIERE URIASA";
-
+			// span.innerHTML = "DESCRIERE URIASA";
+			var tooltip = createTooltip(val.title, 'DESCRIERE URIASA');
 			// divInner.appendChild(span);
 			h3.appendChild(a);
 			div.appendChild(h3);
 			// div.appendChild(divInner);
+			div.appendChild(tooltip);
 			container.appendChild(div);
 		});
+
+        $('[data-toggle="tooltip"]').tooltip();
 	});
 
 	response.fail(function (xhr, status, error) {
