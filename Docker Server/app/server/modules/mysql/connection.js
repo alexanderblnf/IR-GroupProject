5c58ef44abad52f536c0b408ccc4a783584a6331
alexbln <alexandru@g2g-online.nl>
2018-03-28 21:32:35 +0200

    Docker fixes for production

--- Docker Server/app/server/modules/mysql/connection.js
+++ Docker Server/app/server/modules/mysql/connection.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
     host: 'mysql',
     user: 'ir',
-    password: 'irpass',
+    password: 'r5g4XsfNLpU4zuFq',
     database: 'ir_project'
 });
 
